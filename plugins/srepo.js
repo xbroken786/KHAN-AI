1693f272ec261aa9295bdf8c951d6e941fce58c6
Jawad Tech <jawadofcx@gmail.com>
2024-11-29 19:00:33 +0500

    Create srepo.js

---
+++ plugins/srepo.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const { cmd, commands } = require('../command');
+
+cmd({
+    pattern: "srepo",
+    desc: "Fetch information about a GitHub repository.",
+    category: "other",
+    react: "🍃",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const repo = args.join(' ');
+        if (!repo) {
+            return reply("Please provide a GitHub repository name in the format 📌`owner/repo`.");
+        }
+
+        const apiUrl = `https://api.github.com/repos/${repo}`;
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+
+        let repoInfo = `📁_*GITHUB REPO INFO BY KHANX MD*_📁\n\n`;
+        repoInfo += `📌 *ɴᴀᴍᴇ*: ${data.name}\n`;
+        repoInfo += `🔗 *ᴜʀʟ*: ${data.html_url}\n`;
+        repoInfo += `📝 *ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ*: ${data.description}\n`;
+        repoInfo += `⭐ *ꜱᴛᴀʀꜱ*: ${data.stargazers_count}\n`;
+        repoInfo += `🍴 *ꜰᴏʀᴋꜱ*: ${data.forks_count}\n`;
+        repoInfo += `\n`;
+        repoInfo += `> *© Powered By JawadTechX*\n`;
+
+        await conn.sendMessage(from, { text: repoInfo }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`Error fetching repository data🤕: ${e.message}`);
+    }
+});
