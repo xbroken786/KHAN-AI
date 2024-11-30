26766a57b52502fa7b1b2d028be742640b6f2a0b
Jawad Tech <jawadofcx@gmail.com>
2024-11-30 20:22:20 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,6 +9,10 @@ SESSION_ID: process.env.SESSION_ID || "",
   // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
   // add your prifix for bot
+ANTI_LINK: process.env.ANTI_LINK || "false",
+// make anti link true,false for groups 
+ANTI_BAD: process.env.ANTI_BAD || "false",
+// false kr true Anti-Bad words 
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/hzagwo.jpg",
 // add img for alive msg
 ALIVE_MSG: process.env.ALIVE_MSG || "> Zinda Hun Since ${runtime(process.uptime())} ⚡",
@@ -21,6 +25,6 @@ AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   // make true or false status auto seen
 MODE: process.env.MODE || "public",
   // make bot public or private 
-AUTO_VOICE: "false"
+AUTO_VOICE: process.env.AUTO_VOICE || "true"
   // make auto voice false or true
 };
