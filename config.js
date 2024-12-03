88f6560e7f15c44301e6f956b51b47b1c82261ed
Jawad Tech <jawadofcx@gmail.com>
2024-12-03 10:55:11 +0500

    Update config.js

--- config.js
+++ config.js
@@ -13,6 +13,8 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/hzagwo.jpg",
 // add img for alive msg
 ALIVE_MSG: process.env.ALIVE_MSG || "> Zinda Hun Yar *KHAN-AI*⚡",
 // add alive msg here 
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+// Turn true or false for automatic read msgs
 SUDO_NB: process.env.SUDO_NB || "",
 // add your sudo number like 92314xxxxxx
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
@@ -20,13 +22,13 @@ MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNf
 AUTO_REACT: process.env.AUTO_REACT || "true",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
-// false or true for anti bad words 
+// false or true for anti bad words
+ANTI_BAD_WORDS: (process.env.ANTI_BAD_WORDS || "null,sax").split(','),
+// add custom antu bad words 
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 // make true or false status auto seen
 MODE: process.env.MODE || "public",
 // make bot public or private 
-READ_MESSAGE: process.env.READ_MESSAGE || "true",
-// Turn true or false for automatic read msgs
 ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 HEART_REACT: process.env.HEART_REACT || "true",
