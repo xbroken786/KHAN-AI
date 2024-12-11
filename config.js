d8ada57d1e34978a7a551900b6762d9735a4a140
Jawad Tech <jawadofcx@gmail.com>
2024-12-11 16:01:42 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,30 +9,36 @@ SESSION_ID: process.env.SESSION_ID || "",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/hzagwo.jpg",
+BOT_NAME: process.env.BOT_NAME || ".",
+// add bot namw here for menu
+OWNER_NUMBER: process.env.OWNER_NUMBER || "92342758XXXX",
+// add your bot owner number
+OWNER_NAME: process.env.OWNER_NAME || "Jᴀᴡᴀᴅ TᴇᴄʜX",
+// add bot owner name 
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/149k8x.jpg",
 // add img for alive msg
-ALIVE_MSG: process.env.ALIVE_MSG || "> Zinda Hun Yar *KHAN-AI*⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *KHAN-AI*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 // add your mongodb
-AUTO_REACT: process.env.AUTO_REACT || "true",
+AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words
-ANTI_BAD_WORDS: (process.env.ANTI_BAD_WORDS || "null,sax").split(','),
+ANTI_BAD_WORDS: (process.env.ANTI_BAD_WORDS || "null,venom").split(','),
 // add custom antu bad words 
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 // make true or false status auto seen
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
-ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_LINK: process.env.ANTI_LINK || "false",
 // make anti link true,false for groups 
-HEART_REACT: process.env.HEART_REACT || "true",
+HEART_REACT: process.env.HEART_REACT || "false",
 // make this true or false for heart reactions only 
-OWNER_REACT: process.env.OWNER_REACT || "true",
+OWNER_REACT: process.env.OWNER_REACT || "false",
   // make it true or fasle for only react on owner msg only 
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true"
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false"
 // make it true for auto recoding 
 };
