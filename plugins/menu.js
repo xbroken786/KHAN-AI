7ab690d4c83b928dd03c9f284eb78f987f6a15d0
Jawad Tech <jawadofcx@gmail.com>
2024-12-12 09:49:25 +0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -40,7 +40,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• repo
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX`;
+> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX*`;
 
         await conn.sendMessage(
             from,
