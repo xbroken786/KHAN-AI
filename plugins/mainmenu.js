9e528464de86546c1d4532aa4dda4d7a17ec41a5
Jawad Tech <jawadofcx@gmail.com>
2024-12-14 20:07:30 +0500

    Update mainmenu.js

--- plugins/mainmenu.js
+++ plugins/mainmenu.js
@@ -1,3 +1,4 @@
+const config = require('../config')
 const { cmd, commands } = require('../command');
 
 cmd({
@@ -23,7 +24,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• restart
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX`;
+> ${config.FOOTERZ}`;
 
         await conn.sendMessage(
             from,
