854abeb5c364df2ed370ffdbf11cf87a60c8c54d
Jawad Tech <jawadofcx@gmail.com>
2024-12-14 20:05:52 +0500

    Update groupmenu.js

--- plugins/groupmenu.js
+++ plugins/groupmenu.js
@@ -1,3 +1,4 @@
+const config = require('../config')
 const { cmd, commands } = require('../command');
 
 cmd({
@@ -46,7 +47,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• tagadmins
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX`;
+> ${config.FOOTERZ}`;
 
         await conn.sendMessage(
             from,
