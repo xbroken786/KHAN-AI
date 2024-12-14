e2f635ff13895bc3818890f1fb6f4f9c33a2d647
Jawad Tech <jawadofcx@gmail.com>
2024-12-14 20:00:33 +0500

    Update aimenu.js

--- plugins/aimenu.js
+++ plugins/aimenu.js
@@ -1,3 +1,4 @@
+const config = require('../config')
 const { cmd, commands } = require('../command');
 
 cmd({
@@ -20,7 +21,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• copilot
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX`;
+> ${config.FOOTERZ}`;
 
         await conn.sendMessage(
             from,
