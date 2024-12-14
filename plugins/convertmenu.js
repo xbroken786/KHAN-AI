21e793e0caf11d6a9e491a0d4143042d7ebd3694
Jawad Tech <jawadofcx@gmail.com>
2024-12-14 20:01:35 +0500

    Update convertmenu.js

--- plugins/convertmenu.js
+++ plugins/convertmenu.js
@@ -1,3 +1,4 @@
+const config = require('../config')
 const { cmd, commands } = require('../command');
 
 cmd({
@@ -19,7 +20,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• trt
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX`;
+> ${config.FOOTERZ}`;
 
         await conn.sendMessage(
             from,
