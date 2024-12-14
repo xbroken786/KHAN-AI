0c5ffd733d324b8c63f7fe1595d6507b97b91fdb
Jawad Tech <jawadofcx@gmail.com>
2024-12-14 20:05:22 +0500

    Update dlmenu.js

--- plugins/dlmenu.js
+++ plugins/dlmenu.js
@@ -1,3 +1,4 @@
+const config = require('../config')
 const { cmd, commands } = require('../command');
 
 cmd({
@@ -33,7 +34,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• ginisilia 
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX`;
+> ${config.FOOTERZ}`;
 
         await conn.sendMessage(
             from,
