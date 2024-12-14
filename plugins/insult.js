5869a6a03c1bf88d8715ca0dc41d3471b9ac87e3
Jawad Tech <jawadofcx@gmail.com>
2024-12-14 14:30:33 +0500

    Update insult.js

--- plugins/insult.js
+++ plugins/insult.js
@@ -22,3 +22,34 @@ async (Void, citel) => {
     citel.reply(`Error: ${error.message || error}`);
   }
 });
+
+// joke 
+
+cmd({
+    pattern: "joke",
+    desc: "😂 Get a random joke",
+    react: "🤣",
+    category: "fun",
+    filename: __filename
+},
+async (conn, mek, m, { from, q, reply }) => {
+    try {
+        const url = 'https://official-joke-api.appspot.com/random_joke';  // API for random jokes
+        const response = await axios.get(url);
+        const joke = response.data;
+
+        const jokeMessage = `
+😂 *Here's a random joke for you!* 😂
+
+*${joke.setup}*
+
+${joke.punchline} 😄
+
+> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX*`;
+
+        return reply(jokeMessage);
+    } catch (e) {
+        console.log(e);
+        return reply("⚠️ En Error Appears.");
+    }
+});
