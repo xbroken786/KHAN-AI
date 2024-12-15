35e15d3bff286510d1f7c81e64528fdf4e59ab94
Jawad Tech <jawadofcx@gmail.com>
2024-12-15 09:44:15 +0500

    Update dlmenu.js

--- plugins/dlmenu.js
+++ plugins/dlmenu.js
@@ -35,7 +35,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• ginisilia 
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> ${config.FOOTERZ}`;
+> ${config.DESCRIPTION}`;
 
         await conn.sendMessage(
             from,
