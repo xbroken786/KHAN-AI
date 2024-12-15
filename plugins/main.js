96994fdec7cc30a4b509d714e8e209f70da6a5e6
Jawad Tech <jawadofcx@gmail.com>
2024-12-15 09:52:27 +0500

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -214,7 +214,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• ytv
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> ${config.FOOTERZ}`;
+> ${config.DESCRIPTION}`;
 
         await conn.sendMessage(
             from,
