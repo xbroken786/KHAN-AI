7a0733515356432f250635850172ff630bd738ad
Jawad Tech <jawadofcx@gmail.com>
2024-12-15 09:46:50 +0500

    Update groupmenu.js

--- plugins/groupmenu.js
+++ plugins/groupmenu.js
@@ -47,7 +47,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• tagadmins
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> ${config.FOOTERZ}`;
+> ${config.DESCRIPTION}`;
 
         await conn.sendMessage(
             from,
