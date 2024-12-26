1dc6e2517d39d9224bbdb2ce4e4518e4c28c73d9
Jawad Tech <jawadofcx@gmail.com>
2024-12-26 21:15:48 +0500

    Update tools.js

--- plugins/tools.js
+++ plugins/tools.js
@@ -1,6 +1,62 @@
 const axios = require('axios')
 const {cmd , commands} = require('../command')
 
+// Function to format mentions
+const toM = (a) => '@' + a.split('@')[0];
+
+cmd({
+    pattern: "ship",
+    alias: ["cup", "love"],
+    desc: "Randomly pairs the command user with another group member.",
+    react: "❤️",
+    category: "fun",
+    filename: __filename,
+}, 
+async (conn, mek, m, { from, isGroup, groupMetadata, reply }) => {
+    try {
+        // Ensure command is used in a group
+        if (!isGroup) {
+            return reply("This command can only be used in groups.");
+        }
+
+        // Get group participants
+        const participants = groupMetadata.participants.map(p => p.id);
+
+        if (participants.length < 2) {
+            return reply("Not enough members to pair.");
+        }
+
+        // Sender of the command
+        const sender = m.sender;
+
+        // Randomly select another participant
+        let randomParticipant;
+        do {
+            randomParticipant = participants[Math.floor(Math.random() * participants.length)];
+        } while (randomParticipant === sender);
+
+        // Reply with the pairing
+        const message = `${toM(sender)} ❤️ ${toM(randomParticipant)}\nCongratulations 💖🍻`;
+        await conn.sendMessage(from, { 
+            text: message, 
+            mentions: [sender, randomParticipant],
+            contextInfo: {
+                mentionedJid: [m.sender], 
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363354023106228@newsletter',
+                    newsletterName: "JawadTechX",
+                    serverMessageId: 143
+                }
+            }
+        });
+    } catch (e) {
+        console.error("Error in ship command:", e);
+        reply("An error occurred while processing the command. Please try again.");
+    }
+});
+
 // Insult
 
 cmd({
@@ -138,4 +194,5 @@ cmd({
     reply("An error occurred while fetching fonts.");
   }
 });
+
 
