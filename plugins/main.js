60b269d1860b9db67e810f3d531b1155434101fc
Jawad Tech <jawadofcx@gmail.com>
2024-12-28 12:48:11 +0500

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -34,6 +34,9 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• Insta
 ┃◈┃• apk
 ┃◈┃• img
+┃◈┃• tt2
+┃◈┃• git
+┃◈┃• spotify
 ┃◈┃• play
 ┃◈┃• play2
 ┃◈┃• play3
@@ -53,6 +56,9 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ╭━━〔 *Group Menu* 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• grouplink
+┃◈┃• kickall
+┃◈┃• kickall2
+┃◈┃• kickall3
 ┃◈┃• add
 ┃◈┃• remove
 ┃◈┃• kick
@@ -111,6 +117,9 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈╭─────────────·๏
 ┃◈┃• insult
 ┃◈┃• hack
+┃◈┃• ship
+┃◈┃• character
+┃◈┃• pickup 
 ┃◈┃• joke
 ┃◈┃• hrt
 ┃◈┃• hpy
@@ -201,6 +210,10 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ╭━━〔 *Other Menu* 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• fact
+┃◈┃• pair
+┃◈┃• pair2
+┃◈┃• fancy
+┃◈┃• logo <text>
 ┃◈┃• define
 ┃◈┃• news
 ┃◈┃• movie
